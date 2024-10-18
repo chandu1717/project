4099e0937469b94bdbd86f8b3d24e9e0f5622f96
chandu1717 <gchandu103@gmail.com>
2024-10-18 09:53:27 +0530

    ansible

--- ec2.sh
+++ ec2.sh
@@ -5,6 +5,7 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-0b4f379183e5706b9
 SECURITY_GROUP_ID=sg-0c634c5272237c3b6
 DOMAIN_NAME=devrob.online
+HOSTED_ZONE_ID=Z0446877SYHUO28MLWFR
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -20,7 +21,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
@@ -37,3 +38,4 @@ done
 # imporvement
 # check instance is already created or not
 # update route53 record
+
