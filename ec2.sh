1eee328b4505386ea67440268343eda248b8d782
chandu1717 <gchandu103@gmail.com>
2024-10-18 10:04:10 +0530

    ansible

--- ec2.sh
+++ ec2.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
-NAMES=$0
-INSTANCE_TYPE=""
-IMAGE_ID=ami-0b4f379183e5706b9
-SECURITY_GROUP_ID=sg-0c634c5272237c3b6
-DOMAIN_NAME=devrob.online
-HOSTED_ZONE_ID=Z0446877SYHUO28MLWFR
+# Constants
+IMAGE_ID="ami-0b4f379183e5706b9"
+SECURITY_GROUP_ID="sg-0c634c5272237c3b6"
+DOMAIN_NAME="devrob.online"
+HOSTED_ZONE_ID="Z0446877SYHUO28MLWFR"
 
-# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+# Check if at least one argument is provided
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 instance_name1 [instance_name2 ...]"
+    exit 1
+fi
 
-for i in $@
+# Set instance type to t2.micro
+INSTANCE_TYPE="t2.micro"
+
+# Loop through provided instance names
+for i in "$@"
 do  
-    if [[ $i == "mongodb" || $i == "mysql" ]]
-    then
-        INSTANCE_TYPE="t2.micro"
-    else
-        INSTANCE_TYPE="t2.micro"
+    echo "Creating $i instance with type $INSTANCE_TYPE"
+
+    # Create the instance
+    IP_ADDRESS=$(aws ec2 run-instances --image-id "$IMAGE_ID" --instance-type "$INSTANCE_TYPE" --security-group-ids "$SECURITY_GROUP_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    
+    if [ -z "$IP_ADDRESS" ]; then
+        echo "Failed to create instance $i"
+        continue
     fi
-    echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+    echo "Created $i instance: $IP_ADDRESS"
+
+    # Update Route 53 DNS
+    aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --change-batch '
     {
-            "Changes": [{
+        "Changes": [{
             "Action": "CREATE",
-                        "ResourceRecordSet": {
-                            "Name": "'$i.$DOMAIN_NAME'",
-                            "Type": "A",
-                            "TTL": 300,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                        }}]
-    }
-    '
+            "ResourceRecordSet": {
+                "Name": "'"$i.$DOMAIN_NAME"'",
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [{ "Value": "'"$IP_ADDRESS"'" }]
+            }
+        }]
+    }'
 done
-
-# imporvement
-# check instance is already created or not
-# update route53 record
-
